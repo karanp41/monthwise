32f88f200554096d16132d71d00be6171f7ec28e
krishanp-airt <krishanp@airretailer.com>
2025-11-25 20:11:08 +0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MonthWise',
+  webDir: 'dist'
+};
+
+export default config;
